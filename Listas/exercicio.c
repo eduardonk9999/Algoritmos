1fdd4cc3ca8146735fbe5face33897f6f602d761
Eduardo Silva <thiago.novak7@gmail.com>
2023-07-24 09:22:52 -0300

    exercicioLista

---
+++ Listas/exercicio.c
@@ -0,0 +1,11 @@
+Você recebeu a missão de desenvolver mais duas funções para a estrutura de dados do 
+tipo lista simplesmente ligada, a saber, “posicao_menor” e “existe”. Em cada uma delas, 
+você precisará percorrer a lista ligada. Para isso, nós vimos que podemos usar a estrutura de repetição for.
+
+Isso será útil para a função “posicao_menor”, pois temos que percorrer todos os elementos 
+da lista para descobrir qual é a posição do menor elemento, ou seja, nós sabemos 
+a priori quantas iterações serão realizadas. 
+
+Já para a função “existe”, nós podemos usar a estrutura de repetição while, 
+uma vez que não sabemos quantas iterações teremos que fazer para encontrar o 
+elemento desejado, se é que ele existe.
